0f8f53a7830b5edeb41fd16d320dba60976842d7
EidmantasDev <eidmantas.slenys@gmail.com>
2024-04-05 16:33:03 +0300

    To App component adde Meal component In Header component update to use UI/Button component for better styling Created MealItem component to have Meal item logic in one component, used currencyFormatter function to beautify the price Created Meals component to display all the meals coming from backend Created Button component to be used in the application Created util/formatting to format the currency

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <>
       <Header />
+      <Meals />
     </>
   );
 }
