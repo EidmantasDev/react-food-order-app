4872beed5d4223d8e22d0e95df89b9b6d67e73f1
EidmantasDev <eidmantas.slenys@gmail.com>
2024-04-08 18:14:34 +0300

    From CartContext removed console.log Added onClose to Modal component Created Input UI component Created Checkout component In Cart component fixed displaying CartComponent added handleGoToCheckout function to open Checkout modal To App component added Checkout component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -19,21 +19,27 @@ export default function Cart() {
     userProgressCtx.hideCart();
   }
 
+  function handleGoToCheckout() {
+    userProgressCtx.showCheckout();
+  }
+
   return (
-    <Modal className='cart' open={userProgressCtx.progress === 'cart'}>
+    <Modal
+      className='cart'
+      open={userProgressCtx.progress === 'cart'}
+      onClose={userProgressCtx.progress === 'cart' ? handleCloseCart : null}
+    >
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
-          <li key={item.id}>
-            <CartItem
-              key={item.id}
-              name={item.name}
-              quantity={item.quantity}
-              price={item.price}
-              onIncrease={() => cartCtx.addItem(item)}
-              onDecrease={() => cartCtx.removeItem(item.id)}
-            />
-          </li>
+          <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+          />
         ))}
       </ul>
       <p className='cart-total'>{currencyFormatter.format(cartTotal)}</p>
@@ -41,7 +47,9 @@ export default function Cart() {
         <Button textOnly onClick={handleCloseCart}>
           Close
         </Button>
-        <Button onClick={handleCloseCart}>Go to Checkout</Button>
+        {cartCtx.items.length > 0 && (
+          <Button onClick={handleGoToCheckout}>Go to Checkout</Button>
+        )}
       </p>
     </Modal>
   );
